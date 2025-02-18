0082e49f836ca7b9337b91b55343b42755d3e7c5
Harry Hsu <h@stmharry.io>
2025-02-18 13:56:19 +0000

    fix: inference logic and output format

--- main.py
+++ main.py
@@ -5,23 +5,31 @@ from pathlib import Path
 from typing import Any, Literal, TypeAlias
 
 import numpy as np
-import PIL.Image
+import pandas as pd
 from absl import app, flags, logging
 from detectron2.config import CfgNode, get_cfg
 from detectron2.engine import DefaultPredictor
 from detectron2.projects.deeplab import add_deeplab_config
+from PIL import Image
 from ultralytics.engine.results import Results
 from ultralytics.models.yolo.segment import SegmentationPredictor
 from ultralytics.utils.ops import scale_masks
 
-flags.DEFINE_multi_string("input", None, "Input file paths or glob patterns.")
-flags.DEFINE_string("output", None, "Output directory.")
-flags.DEFINE_string("deeplab_config", None, "DeepLab config file.")
-flags.DEFINE_string("deeplab_weights", None, "DeepLab weights file.")
-flags.DEFINE_string("yolo_config", None, "YOLO config file.")
-flags.DEFINE_string("yolo_weights", None, "YOLO weights file.")
+flags.DEFINE_multi_string("input", "./data/*", "Input file paths or glob patterns.")
+flags.DEFINE_string("output", "./results", "Output directory.")
+flags.DEFINE_string(
+    "deeplab_config", "./models/deeplab/config.yaml", "DeepLab config file."
+)
+flags.DEFINE_string(
+    "deeplab_weights", "./models/deeplab/model.pth", "DeepLab weights file."
+)
+flags.DEFINE_string("yolo_config", "./models/yolo/config.yaml", "YOLO config file.")
+flags.DEFINE_string("yolo_weights", "./models/yolo/model.pt", "YOLO weights file.")
 flags.DEFINE_string("device", "cpu", "Device to use.")
 FLAGS = flags.FLAGS
+
+
+# semantic segmentation
 
 
 @dataclasses.dataclass(kw_only=True)
@@ -35,8 +43,6 @@ class SemanticSegmentationModule(object):
     weights_path: str
     device: str = "cpu"
 
-    batch_size: int = 1
-
     predictor: DefaultPredictor = dataclasses.field(init=False)
 
     def __post_init__(self):
@@ -45,19 +51,21 @@ class SemanticSegmentationModule(object):
         cfg.merge_from_file(self.config_path)
 
         cfg.DATASETS.TEST = ()
+        cfg.INPUT.FORMAT = "RGB"
         cfg.INPUT.CROP.ENABLED = False
         cfg.MODEL.DEVICE = self.device
         cfg.MODEL.WEIGHTS = self.weights_path
-        cfg.SOLVER.IMS_PER_BATCH = self.batch_size
-        assert cfg.INPUT.FORMAT == "RGB"
+        cfg.SOLVER.IMS_PER_BATCH = 1
 
         self.predictor = DefaultPredictor(cfg)
 
     def __call__(self, image: np.ndarray) -> SemanticSegmentationPrediction:
-        prob = self.predictor(image)["sem_seg"].softmax(dim=0).numpy()
+        prob: np.ndarray = self.predictor(image)["sem_seg"].softmax(dim=0).numpy()
 
         return SemanticSegmentationPrediction(prob=prob)
 
+
+# instance detection
 
 IntersectionMetric: TypeAlias = Literal[
     "intersection_over_union",
@@ -69,11 +77,23 @@ IntersectionMetric: TypeAlias = Literal[
 class InstanceDetectionPredictionInstance(object):
     score: float
     category_id: int
-    bbox_xywh: np.ndarray
+    category_name: str
+    bbox_xyxy: np.ndarray
     mask: np.ndarray
 
-    def __repr__(self) -> str:
-        return f"<Instance: cls={self.category_id} score={self.score:.5f}>"
+    @property
+    def bbox_xywh(self) -> np.ndarray:
+        xb, yb, xe, ye = self.bbox_xyxy
+        return np.array([(xb + xe) // 2, (yb + ye) // 2, xe - xb, ye - yb])
+
+    @property
+    def bbox_slices(self) -> tuple[slice, ...]:
+        xb, yb, xe, ye = self.bbox_xyxy
+        return (slice(yb, ye + 1), slice(xb, xe + 1))
+
+    @property
+    def mask_area(self) -> int:
+        return self.mask[self.bbox_slices].sum()
 
     def intersect(
         self,
@@ -82,11 +102,8 @@ class InstanceDetectionPredictionInstance(object):
         epsilon: float = 1e-5,
     ) -> float:
 
-        xb1, yb1, w1, h1 = self.bbox_xywh
-        xb2, yb2, w2, h2 = other.bbox_xywh
-
-        xe1, ye1 = xb1 + w1, yb1 + h1
-        xe2, ye2 = xb2 + w2, yb2 + h2
+        xb1, yb1, xe1, ye1 = self.bbox_xyxy
+        xb2, yb2, xe2, ye2 = other.bbox_xyxy
 
         bbox_intersection_slices: tuple[slice, ...] = (
             slice(max(yb1, yb2), min(ye1, ye2) + 1),
@@ -123,21 +140,14 @@ class InstanceDetectionPredictionInstance(object):
                 f"Shape incompatible: {other.shape} v.s. {self.mask.shape}"
             )
 
-        x, y, w, h = self.bbox_xywh
-        slices: tuple[slice, ...] = (
-            slice(y, y + h + 1),
-            slice(x, x + w + 1),
-        )
-
-        other = other[(..., *slices)]
-        mask = self.mask[slices]
+        other = other[(..., *self.bbox_slices)]
+        mask = self.mask[self.bbox_slices]
 
         return other[..., mask]
 
 
 @dataclasses.dataclass(kw_only=True)
 class InstanceDetectionPrediction(object):
-    category_id_to_name: dict[int, str]
     instances: list[InstanceDetectionPredictionInstance]
 
     @classmethod
@@ -147,57 +157,55 @@ class InstanceDetectionPrediction(object):
         assert results.boxes is not None
         assert results.masks is not None
 
-        masks = scale_masks(results.masks.data[None, ...], shape=results.orig_shape)[0]
+        masks = scale_masks(
+            results.masks.data[None, ...], shape=results.orig_shape
+        ).squeeze(dim=0)
 
         instances: list[InstanceDetectionPredictionInstance] = [
             InstanceDetectionPredictionInstance(
                 score=float(score),
                 category_id=int(category_id),
-                bbox_xywh=bbox_xywh.numpy().astype(int),
+                category_name=results.names[int(category_id)],
+                bbox_xyxy=bbox_xyxy.numpy().astype(int),
                 mask=mask.numpy().astype(bool),
             )
-            for score, category_id, bbox_xywh, mask in zip(
+            for score, category_id, bbox_xyxy, mask in zip(
                 results.boxes.conf,
                 results.boxes.cls,
-                results.boxes.xywh,
+                results.boxes.xyxy,
                 masks,
             )
         ]
 
-        return cls(
-            category_id_to_name=results.names,
-            instances=instances,
-        )
+        return cls(instances=instances)
 
     def sort_by_score(
-        self,
-        direction: Literal["ASCENDING", "DESCENDING"] = "DESCENDING",
+        self, direction: Literal["ASCENDING", "DESCENDING"] = "DESCENDING"
     ) -> "InstanceDetectionPrediction":
-
-        instances: list[InstanceDetectionPredictionInstance] = sorted(
-            self.instances,
-            key=lambda instance: instance.score,
-            reverse=(direction == "DESCENDING"),
-        )
         return InstanceDetectionPrediction(
-            category_id_to_name=self.category_id_to_name,
-            instances=instances,
+            instances=sorted(
+                self.instances,
+                key=lambda instance: instance.score,
+                reverse=(direction == "DESCENDING"),
+            ),
         )
 
-    def filter_by_category(
-        self,
-        regex: Any,
-    ) -> "InstanceDetectionPrediction":
-
-        instances: list[InstanceDetectionPredictionInstance] = [
-            instance
-            for instance in self.instances
-            if re.match(regex, self.category_id_to_name[instance.category_id])
-        ]
-
+    def filter_by_category(self, regex: Any) -> "InstanceDetectionPrediction":
         return InstanceDetectionPrediction(
-            category_id_to_name=self.category_id_to_name,
-            instances=instances,
+            instances=[
+                instance
+                for instance in self.instances
+                if re.match(regex, instance.category_name)
+            ],
+        )
+
+    def filter_by_area(self, threshold: int = 0) -> "InstanceDetectionPrediction":
+        return InstanceDetectionPrediction(
+            instances=[
+                instance
+                for instance in self.instances
+                if instance.mask_area > threshold
+            ],
         )
 
     def non_maximum_suppression(
@@ -222,22 +230,19 @@ class InstanceDetectionPrediction(object):
             instances = _instances
             max_instances.append(instance)
 
-        return InstanceDetectionPrediction(
-            category_id_to_name=self.category_id_to_name,
-            instances=max_instances,
-        )
+        return InstanceDetectionPrediction(instances=max_instances)
 
 
 @dataclasses.dataclass(kw_only=True)
 class InstanceDetectionModule(object):
     config_path: str
     weights_path: str
-    device: str = "cpu"
 
-    batch_size: int = 1
     conf: float = 0.0001
-    iou: float = 0.7
+    iou: float = 0.5
     max_det: int = 500
+    retina_masks: bool = True
+    device: str = "cpu"
 
     predictor: SegmentationPredictor = dataclasses.field(init=False)
 
@@ -247,12 +252,14 @@ class InstanceDetectionModule(object):
             overrides={
                 "mode": "predict",
                 "model": self.weights_path,
-                "batch": self.batch_size,
+                "batch": 1,
                 "conf": self.conf,
                 "iou": self.iou,
                 "max_det": self.max_det,
+                "retina_masks": self.retina_masks,
                 "show": False,
                 "save": False,
+                "device": self.device,
                 "verbose": False,
             },
         )
@@ -265,6 +272,9 @@ class InstanceDetectionModule(object):
         results: Results = list(self.predictor(image))[0]  # type: ignore
 
         return InstanceDetectionPrediction.from_ultralytics_results(results)
+
+
+# post-processing
 
 
 class FindingType(str, Enum):
@@ -280,30 +290,30 @@ class FindingType(str, Enum):
 
 @dataclasses.dataclass(kw_only=True)
 class FindingEntry(object):
-    uns_index: int
+    index: int
     finding: FindingType
     score: float
+
+    @property
+    def fdi(self) -> str:
+        quadrant: int = (self.index - 1) // 8 + 1
+        tooth: int = (self.index - 1) % 8 + 1
+
+        return f"{quadrant}{tooth}"
 
 
 @dataclasses.dataclass(kw_only=True)
 class FindingAssessment(object):
-    file_name: str
+    name: str
     entries: list[FindingEntry]
 
-    def __post_init__(self):
-        keys: set[tuple[int, FindingType]] = set()
-        for entry in self.entries:
-            key: tuple[int, FindingType] = (entry.uns_index, entry.finding)
-            if key in keys:
-                raise ValueError(f"Duplicate entry: {entry}")
-
-            keys.add(key)
-
-    def to_csv(self, path: Path):
-        with open(path, "w") as file:
-            file.write("file_name,finding,score\n")
-            for entry in self.entries:
-                file.write(f"{self.file_name},{entry.finding.name},{entry.score}\n")
+    def to_csv(self, path: str | Path) -> None:
+        pd.DataFrame({
+            "file_name": self.name,
+            "fdi": [entry.fdi for entry in self.entries],
+            "finding": [entry.finding.name for entry in self.entries],
+            "score": [entry.score for entry in self.entries],
+        }).to_csv(path, index=False)
 
 
 @dataclasses.dataclass(kw_only=True)
@@ -315,29 +325,33 @@ class PostProcessingModule(object):
         self,
         semseg_pred: SemanticSegmentationPrediction,
         insdet_pred: InstanceDetectionPrediction,
-        file_name: str,
-    ) -> FindingAssessment:
+    ) -> list[FindingEntry]:
 
-        # semseg_prob.shape = (C, H, W)
         semseg_prob: np.ndarray = semseg_pred.prob
 
         finding_entries: list[FindingEntry] = []
         for finding in FindingType:
+
             instances: list[InstanceDetectionPredictionInstance]
             match finding:
                 case FindingType.MISSING:
-                    # for tooth objects, additional nms is applied because in instance detection
-                    # we treat each fdi as a separate class
+                    # NOTE: for tooth objects, additional nms is applied because in
+                    #   instance detection we treat each fdi as a separate class
                     instances = (
                         insdet_pred.filter_by_category("^TOOTH")
                         .non_maximum_suppression(
                             method=self.nms_method, threshold=self.nms_threshold
                         )
+                        .filter_by_area()
                         .instances
                     )
 
                 case _:
-                    instances = insdet_pred.filter_by_category(f"^{finding}$").instances
+                    instances = (
+                        insdet_pred.filter_by_category(f"^{finding.value}$")
+                        .filter_by_area()
+                        .instances
+                    )
 
             prob_discount: np.ndarray | None = None
             if len(instances):
@@ -347,11 +361,13 @@ class PostProcessingModule(object):
 
             finding_scores: list[np.ndarray] = []
             for instance in instances:
+                assert prob_discount is not None
+
                 semseg_prob_masked: np.ndarray = instance.mask_array(semseg_prob)
                 prob_discount_masked: np.ndarray = instance.mask_array(prob_discount)
 
-                semseg_prob_masked: np.ndarray = 1 - np.power(
-                    1 - semseg_prob_masked, 1 / prob_discount_masked
+                semseg_prob_masked = 1.0 - np.power(
+                    1.0 - semseg_prob_masked, 1 / prob_discount_masked
                 )
                 if semseg_prob_masked.size == 0:
                     continue
@@ -377,17 +393,24 @@ class PostProcessingModule(object):
             else:
                 finding_score = np.zeros((len(semseg_prob),), dtype=np.float32)
 
+            match finding:
+                case FindingType.MISSING:
+                    finding_score = 1 - finding_score
+
+                case _:
+                    ...
+
             for index, _finding_score in enumerate(finding_score):
                 # the background class is ignored
                 if index == 0:
                     continue
 
                 finding_entry: FindingEntry = FindingEntry(
-                    uns_index=index, finding=finding, score=_finding_score
+                    index=index, finding=finding, score=_finding_score
                 )
                 finding_entries.append(finding_entry)
 
-        return FindingAssessment(entries=finding_entries, file_name=file_name)
+        return finding_entries
 
 
 def main(_):
@@ -422,19 +445,20 @@ def main(_):
 
     # processing
 
-    for image_path in image_paths:
+    for image_path in sorted(image_paths):
         csv_path: Path = Path(output_dir, image_path.with_suffix(".csv").name)
         logging.info(f"Processing {image_path}, saving to {csv_path}")
 
-        image_pil: PIL.Image.Image = PIL.Image.open(image_path).convert("RGB")
+        image_pil: Image.Image = Image.open(image_path).convert("RGB")
         image: np.ndarray = np.asarray(image_pil)
 
         semseg_pred: SemanticSegmentationPrediction = semseg_module(image)
         insdet_pred: InstanceDetectionPrediction = insdet_module(image)
-        assessment: FindingAssessment = postproc_module(
-            semseg_pred, insdet_pred, file_name=image_path.name
-        )
+        finding_entries: list[FindingEntry] = postproc_module(semseg_pred, insdet_pred)
 
+        assessment: FindingAssessment = FindingAssessment(
+            name=image_path.stem, entries=finding_entries
+        )
         assessment.to_csv(csv_path)
 
 
